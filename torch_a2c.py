8e4fb1866d0f8808c5da96534982a4dde303f448
Eric Z. Beard <ericzbeard@gmail.com>
2022-03-07 10:55:59 -0800

    a2c note

--- torch_a2c.py
+++ torch_a2c.py
@@ -1,5 +1,14 @@
 """
 Pytorch stable baselines 3 A2C model.
+
+This algorithm is the fastest I have tried so far. It can learn at a rate of 
+400-500 steps per second. 
+
+At 1,000,000 steps, it converges on a single word and assumes that's always right, 
+probably because it only sees a few positive rewards. There are 11M possible actions, 
+and to learn english words an algorithm would need to try them all several times, 
+so we need to do a few hundred million steps, with very slow learning.
+
 """
 import gym
 
